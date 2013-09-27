1eb64fe64a3a4891b3326a3576ffa434b79a7c7d
Varun <varunkuamr.saini06@gmail.com>
2013-09-27 07:05:28 -0500

    Added Notebook theme
    Added Notebook theme

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ $(document).ready(function() {
             $('input#btn').trigger('click');
         }
     });
-    
+
 	$('input#btn').on('click', function(){
 			event.preventDefault();
-			
+
 			/* this tests if no text or whitespace was entered */
 			if(!$.trim($(this).val())){
 				$('input#add').val('Item');
 			}
-			
-			$('li#base').clone(true).appendTo('#sortable').removeAttr('id').removeClass('hidden');
-			$('ul#sortable>li:last>form>span').text($('input#add').val());
+
+			$('li#base').clone(true).appendTo('#item_list').removeAttr('id').removeClass('hidden');
+			$('ul#item_list>li:last>form>span').text($('input#add').val());
 			$('input#add').val("");
 
 			var doc_height = $(document).height();
 			var win_height = $(window).height();
-			
+
 			if(doc_height > win_height){
 				$('div#container').css('height', 'auto');
 			}
@@ -30,21 +30,12 @@ $(document).ready(function() {
 /*Checkbox Strikethrough Item Text */
 	$('input.check').change(function(){
 		$(this).siblings('.item').toggleClass('strike');
-		
-		/* This pushes checked items to the bottom of the list */
-		/* --This feature was removed since users found it 
-		jarring when the item they were looking at moved
-		
-		if($(this).siblings('.item').hasClass('strike')){
-			$(this).closest('li').appendTo('ul#sortable');
-		}
-		*/
 	});
-	
+
 /* Removing Checked Items */
-	$('form#remove-check').submit(function(event){
+	$('form#remove-item').submit(function(event){
 		event.preventDefault();
-		$('ul#sortable li').each(function(){
+		$('ul#item_list li').each(function(){
 			if($(this).find('.item').hasClass('strike')){
 				$(this).remove();
 			}
