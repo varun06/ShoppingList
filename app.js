8ba4ace31fea9a882bd465b6a68e15608d7221ec
Varun <varunkuamr.saini06@gmail.com>
2013-09-29 06:53:01 -0500

    Code Refactoring and added logo
    Code Refactoring and added logo

--- app.js
+++ app.js
@@ -1,52 +1,44 @@
 $(document).ready(function() {
-/* Adding Items */
+/* Get user input */
+ getItem();
+
+	function getItem(){
     $('input#add').keydown(function(event){
         if(event.keyCode==13){
-            $('input#btn').trigger('click');
+            addItem();
         }
     });
+  }
 
-	$('input#btn').on('click', function(){
-			event.preventDefault();
+	function addItem(){
 
-			/* this tests if no text or whitespace was entered */
-			if(!$.trim($(this).val())){
-				$('input#add').val('Item');
-			}
+		if(!$.trim($('#add').val())){
+			$('input#add').val('Item');
+		}
 
-			$('li#base').clone(true).appendTo('#item_list').removeAttr('id').removeClass('hidden');
-			$('ul#item_list>li:last>form>span').text($('input#add').val());
-			$('input#add').val("");
+		$('li#base').clone(true).appendTo('#item_list').removeAttr('id').removeClass('hidden');
+		$('ul#item_list>li:last>form>span').text($('input#add').val());
+		$('input#add').val("");
+		addDeleteButton();
+	}
 
-			var doc_height = $(document).height();
-			var win_height = $(window).height();
+	function addDeleteButton(){
+			$('input.check').change(function () {
+	    	$(this).siblings('.item').toggleClass('strike', this.checked);
+	    	$(this).closest('li').find('.delete_item').toggleClass('hidden', !this.checked);
+			});
+			removeItem();
+		}
 
-			if(doc_height > win_height){
-				$('div#container').css('height', 'auto');
-			}
-	});
-
-	$('input.check').change(function () {
-    $(this).siblings('.item').toggleClass('strike', this.checked);
-    $(this).closest('li').find('.delete_item').toggleClass('hidden', !this.checked);
-});
-
-// Remove list item when button is clicked
-	$('.delete_item').on('click', function(){
+	// Remove list item when button is clicked
+	function removeItem(){
+		$('.delete_item').on('click', function(){
 			$('ul#item_list li').each(function(){
-			if($(this).find('.item').hasClass('strike')){
-				$(this).remove();
-			}
+				if($(this).find('.item').hasClass('strike')){
+					$(this).remove();
+					// $(this).fadeOut(300);
+				}
+			});
 		});
-	});
-
-/* Removing Checked Items */
-	// $('form#remove-item').submit(function(event){
-	// 	event.preventDefault();
-	// 	$('ul#item_list li').each(function(){
-	// 		if($(this).find('.item').hasClass('strike')){
-	// 			$(this).remove();
-	// 		}
-	// 	});
-	// });
+	}
 });
