e47b07426904d56448ed1b2a6d84e7b1601090f7
Varun <varunkuamr.saini06@gmail.com>
2013-09-30 18:12:06 -0500

    Some Style changes
    Some Style changes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-	/* Get user input */
+
 	getItem();
 
+	/* Get user input and call addItem function*/
 	function getItem(){
     $('input#add').keydown(function(event){
         if(event.keyCode==13){
@@ -13,6 +14,7 @@ $(document).ready(function() {
     });
   }
 
+  // This function add the items to list
 	function addItem(){
 
 		if(!$.trim($('#add').val())){
@@ -21,17 +23,18 @@ $(document).ready(function() {
 
 		$('li#base').clone(true).appendTo('#item_list').removeAttr('id').removeClass('hidden');
 		$('ul#item_list>li:last>form>span').text($('input#add').val());
-		$('input#add').val("");
-		addDeleteButton();
+		$('input#add').val(""); //set the text field to blank
+		addDeleteButton(); //Call addDeleteButton
 	}
 
+	// This function adds a delete button when user check the checkbox
 	function addDeleteButton(){
 			$('input.check').change(function () {
-	    	$(this).siblings('.item').toggleClass('strike', this.checked);
-	    	$(this).closest('li').find('.delete_item').toggleClass('hidden', !this.checked);
-			});
-			removeItem();
-		}
+	   	$(this).siblings('.item').toggleClass('strike', this.checked);
+	   	$(this).closest('li').find('.delete_item').toggleClass('hidden', !this.checked);
+		});
+		removeItem(); //Call removeItem function
+	}
 
 	// Remove list item when button is clicked
 	function removeItem(){
@@ -43,4 +46,4 @@ $(document).ready(function() {
 			});
 		});
 	}
-});
+}); // end here
